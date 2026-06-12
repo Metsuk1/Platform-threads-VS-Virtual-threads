93176be00a6e2cb136cbea174d91fd6b0918e3ae
Abayev <240918@astanait.edu.kz>
2026-06-12 21:18:23 +0500

    add smth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,37 @@
 package org.example;
 
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    private static volatile int increment = 0;
+
+    public static    int increment() {
+        return increment++;
+    }
+
+    public static void main(String[] args) {
+        int numberOfThreads = 10_000;
+        List<Thread> threads = new ArrayList<>(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            Thread t = new Thread(() -> {
+                increment();
+            });
+            threads.add(t);
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("Ожидаемый результат: " + numberOfThreads);
+        System.out.println("Фактический результат: " + increment);
     }
 }
